58a5709ca9272c0ef26f353c20543fe8ceb399b1
Andrew James Booth <booan450@student.otago.ac.nz>
2023-09-22 12:23:43 +1200

    added terrain

--- Assets/Scipts/playerController.cs
+++ Assets/Scipts/playerController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent (typeof (Rigidbody))]
 public class playerController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody myRigidbody;
-    private int lives = 3;
+    private int lives = 4;
+    private float health = 1.0f; // Start with full health (1.0)
+
+    public Slider healthSlider; // Reference to your Health Bar Slider
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthSlider = GameObject.Find("Canvas/Health_Bar").GetComponent<Slider>();
+        UpdateHealthBar();
     }
 
     // Update is called once per frame
@@ -23,12 +28,26 @@ public class playerController : MonoBehaviour
         myRigidbody.MovePosition (myRigidbody.position + velocity * Time.fixedDeltaTime);
     }
 
-    private void OnTriggerEnter(Collider other){
+    private void OnTriggerEnter(Collider other) {
+        // Reduce health when taking damage
+        health -= 0.25f; // Adjust this value based on your game's health system
         lives -= 1;
-        print("life lost");
+        print("Damage taken");
         Destroy(other.transform.parent.gameObject);
-        if (lives < 0) {
+
+        // Check for death
+        if (health <= 0) {
             Destroy(gameObject);
-        }   
+        }
+
+        UpdateHealthBar(); // Call the function to update the Health Bar
+    }
+
+
+    void UpdateHealthBar() {
+        // Update the Health Bar Slider with the current health value
+        if (healthSlider != null) {
+            healthSlider.value = health;
+        }
     }
 }
