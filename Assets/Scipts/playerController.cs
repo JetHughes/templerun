ad203dd0871e2b24546e6aa491629da8f2bc4308
Jet Hughes <hugje043@student.otago.ac.nz>
2023-09-27 12:20:02 +1300

    added horizontal blocks and coins

--- Assets/Scipts/playerController.cs
+++ Assets/Scipts/playerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 [RequireComponent (typeof (Rigidbody))]
 public class playerController : MonoBehaviour
@@ -13,12 +14,16 @@ public class playerController : MonoBehaviour
     private float health = 1.0f; // Start with full health (1.0)
 
     Slider healthSlider; // Reference to your Health Bar Slider
+    public TMP_Text coinsLabel;
+    private int coins = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         healthSlider = GameObject.Find("Canvas/Health_Bar").GetComponent<Slider>();
+        coinsLabel = GameObject.Find("Canvas/Coins_Label").GetComponent<TMP_Text>();
         UpdateHealthBar();
+        UpdateCoins();
         GameOver.SetActive(false);
     }
 
@@ -31,7 +36,7 @@ public class playerController : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
-        // if (other.tag == "obstacle"){
+        if (other.tag == "obstacle"){
             // Reduce health when taking damage
             health -= 0.25f; // Adjust this value based on your game's health system
             lives -= 1;
@@ -43,11 +48,17 @@ public class playerController : MonoBehaviour
                 Destroy(gameObject);
                 GameOver.SetActive(true);
             }
-        // } else if (other.tag == "coin"){
-
-        // }
-
+        } else if (other.tag == "coin"){
+            coins += 1;
+        }
+        UpdateCoins();
         UpdateHealthBar(); // Call the function to update the Health Bar
+    }
+
+    void UpdateCoins(){
+        if (coinsLabel != null){
+            coinsLabel.SetText(coins.ToString());
+        }
     }
 
 
